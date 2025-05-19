72ce8579494115f48cc8e269e3fe422c94690926
SaktiPadmayoga <sakti14yoga@gmail.com>
2025-05-19 23:12:12 +0700

    update event route

---
+++ routes/eventRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const {
+  getEvents,
+  getEventById,
+  createEvent,
+  updateEvent,
+  deleteEvent
+} = require('../controllers/eventController');
+const { protect } = require('../middlewares/authMiddleware');
+
+router.get('/', protect, getEvents);
+router.get('/:id', protect, getEventById);
+router.post('/', protect, createEvent);
+router.put('/:id', protect, updateEvent);
+router.delete('/:id', protect, deleteEvent);
+router.get('/calendar', protect, getCalendarEvents);
+
+
+module.exports = router;
