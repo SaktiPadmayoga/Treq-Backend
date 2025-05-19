f5cd18d2bd231dc7ab417fdb96c74fff139f9b29
SaktiPadmayoga <sakti14yoga@gmail.com>
2025-05-19 23:12:37 +0700

    update task route

---
+++ routes/taskRoutes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const {
+  createTask,
+  getTasks,
+  getTaskById,
+  updateTask,
+  deleteTask,
+  getMyTaskProgress
+} = require('../controllers/taskController');
+const { protect } = require('../middlewares/authMiddleware');
+
+const router = express.Router();
+
+router.route('/')
+  .post(protect, createTask)
+  .get(protect, getTasks);
+
+router.route('/progress')
+  .get(protect, getMyTaskProgress);
+
+router.route('/:id')
+  .get(protect, getTaskById)
+  .put(protect, updateTask)
+  .delete(protect, deleteTask);
+
+router.patch('/:id/status', protect, updateTaskStatus);
+router.patch('/:id/checklist', protect, updateTaskChecklist);
+router.get('/dashboard/summary', protect, getDashboardData);
+
+
+module.exports = router;
